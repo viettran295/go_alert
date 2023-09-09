c867cf0a644a999f9c0636bbbbb65a3dd71dc7c8
viettr <viettran295@gmail.com>
2023-09-09 21:28:34 +0200

    Add kafka consumer cfg

--- util/config.go
+++ util/config.go
@@ -2,20 +2,21 @@ package util
 
 import "github.com/spf13/viper"
 
-type Config struct{
-	EmailSenderAddress string `mapstructure:"EMAIL_SENDER_ADDRESS"`
+type Config struct {
+	EmailSenderAddress  string `mapstructure:"EMAIL_SENDER_ADDRESS"`
 	EmailSenderPassword string `mapstructure:"EMAIL_SENDER_PASSWORD"`
 
 	KafkaBootstrapServer string `mapstructure:"KAFKA_BOOTSTRAPSERVER"`
-	KafkaGroupid string `mapstructure:"GROUP_Id"`
+	ProducerGroupid      string `mapstructure:"PRODUCER_GROUP_Id"`
+	ConsumerGroupid      string `mapstructure:"CONSUMER_GROUP_ID"`
 }
 
-func LoadConfig(path string) (config Config, err error){
+func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
 	err = viper.ReadInConfig()
-	if err != nil{
+	if err != nil {
 		return
 	}
 	err = viper.Unmarshal(&config)
