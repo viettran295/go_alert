ca0ab7d74d434962b041f444b6e527fd923f1ab3
viettr <viettran295@gmail.com>
2023-11-05 21:10:02 +0100

    Test a single func

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
-	"log"
-	"main/mail_alert"
-	"main/util"
+	// kafka "go_alert/go_kafka"
+	// "go_alert/util"
+	// "log"
+	"fmt"
+	trend "go_alert/chart_trend"
 )
 
-func main() {
-	cfg, error := util.LoadConfig(".")
-	if error != nil{
-		log.Println("Error:", error)
-	}
-	emailSender := mail_alert.NewGmailSender(cfg.EmailSenderAddress, cfg.EmailSenderPassword)
-	From := "vietpride295@gmail.com"
-	To := []string{"viettran295@gmail.com"}
-	Subject, Text := "Go test", "Test Go"
-	emailSender.SendEmail(From, To, Subject, []byte(Text))
+var CryptoAPISrc = trend.APISource{
+	Url:    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest",
+	Method: "GET",
+}
 
+func main() {
+	data := trend.GetPrice("btc", CryptoAPISrc)
+	fmt.Println(data)
+
+	// cfg, err_cfg := util.LoadConfig(".")
+	// <<<<<<< Send Email >>>>>>>
+	// if error != nil{
+	// 	log.Println("Error:", error)
+	// }
+	// emailSender := go_mail.NewGmailSender(cfg.EmailSenderAddress, cfg.EmailSenderPassword)
+	// From := "vietpride295@gmail.com"
+	// To := []string{"viettran295@gmail.com"}
+	// Subject, Text := "Go test", "Test Go"
+	// emailSender.SendEmail(From, To, Subject, []byte(Text))
+
+	// <<<<<<< ETH >>>>>>
+	// client, err := ethclient.DialContext(context.Background(), infuraURL)
+	// if err != nil{
+	// 	log.Println(err)
+	// }
+	// defer client.Close()
+	// block, err := client.BlockByNumber(context.Background(), nil)
+	// log.Print(block.Number())
+
+	// <<<<<< Kafka >>>>>>
+	// if err_cfg != nil {
+	// 	log.Println("Can not load config info")
+	// }
+	// consumer := kafka.NewConsumer(cfg.KafkaBootstrapServer, cfg.ConsumerGroupid)
+	// kafka.Kafka_consume(consumer, []string{"saigon"})
 }
